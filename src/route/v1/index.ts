eea7c22ab4e77c8c0a2af1727770067faa09a5b8
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-07 01:56:39 +0900

    feat: add user info

--- src/route/v1/index.ts
+++ src/route/v1/index.ts
@@ -7,6 +7,16 @@ router.get('/', (req: express.Request, res: express.Response) => {
   res.send('server is up');
 });
 
+router.get('/users/:userId', (req: express.Request, res: express.Response) => {
+  twitter.get('users/show', { screen_name: req.params.userId }, (error: Error, data: any) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
 router.get('/favorites/:userId', (req: express.Request, res: express.Response) => {
   twitter.get('favorites/list', { screen_name: req.params.userId }, (error: Error, data: any) => {
     if (error) {
