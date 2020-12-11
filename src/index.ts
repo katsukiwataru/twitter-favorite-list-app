18a7d5fedbaa8abae11d538ba17dd6508e9ed955
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-12-12 05:44:45 +0900

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import fs from 'fs';
 import https from 'https';
@@ -6,6 +7,7 @@ import { router } from './route/v1';
 
 const app: express.Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
